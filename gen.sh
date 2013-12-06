64bb516263c7117626c209a793d1889d944b80d5
Ben Webb <bjwebb67@googlemail.com>
2013-12-06 15:12:35 +0000

    Change github url from ssh to https

--- gen.sh
+++ gen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf IATI-Codelists-External combined-xml
-git clone git@github.com:IATI/IATI-Codelists-External.git
+git clone https://github.com/IATI/IATI-Codelists-External.git
 
 mkdir combined-xml
 cp xml/* combined-xml
