0c5c4ebbf784ee6f11a7832e4091efc156c452c7
Ben Webb <bjwebb67@googlemail.com>
2014-03-31 18:56:50 +0100

    [#8] Output conditions in JSON mapping file

--- mappings_to_json.py
+++ mappings_to_json.py
@@ -3,10 +3,13 @@ import os, json
 
 def mapping_to_json(mappings):
     for mapping in mappings.getroot().xpath('//mapping'):
-        yield {
+        out = {
             'path': mapping.find('path').text,
             'codelist': mapping.find('codelist').attrib['ref']
         }
+        if mapping.find('condition') is not None:
+            out['condition'] = mapping.find('condition').text
+        yield out
 
 mappings = ET.parse('mapping.xml')
 with open('mapping.json', 'w') as fp:
