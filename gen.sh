e4b547cbc04c1dce778497aff8dffa567cf12f8f
Dale Potter <dalepotter@users.noreply.github.com>
2015-11-25 14:52:02 +0000

    Switch branch of NE codelists for v2.02 testing

--- gen.sh
+++ gen.sh
@@ -3,12 +3,12 @@
 rm -rf combined-xml
 if [ -d IATI-Codelists-NonEmbedded ]; then
     cd IATI-Codelists-NonEmbedded || exit 1
-    git checkout master
+    git checkout version-2.02-additions
     git pull
 else
     git clone https://github.com/IATI/IATI-Codelists-NonEmbedded.git
     cd IATI-Codelists-NonEmbedded || exit 1
-    git checkout master
+    git checkout version-2.02-additions
 fi
 cd .. || exit 1
 
