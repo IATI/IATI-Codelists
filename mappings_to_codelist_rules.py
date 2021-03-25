56ed70b06df81678966ea4c0042cdc816d1c9f6d
nosvalds <niko@devinit.org>
2021-03-25 15:55:18 +0000

    update format to handle more cases

--- mappings_to_codelist_rules.py
+++ mappings_to_codelist_rules.py
@@ -2,16 +2,21 @@ import os
 import json
 from lxml import etree as ET
 
-attrkey = '$'
-
 def mapping_to_codelist_rules(mappings):
+    data = dict()
     for mapping in mappings.getroot().xpath('//mapping'):
-        path_ref = mapping.find('path').text.split('@')
+        path_ref = mapping.find('path').text.split('/@')
         # handle edge case of path: '//iati-activity/crs-add/channel-code/text()'
         if len(path_ref) != 2:
             split = mapping.find('path').text.rpartition('/')
-            path_ref = [split[0] + split[1], split[2]]
+            path_ref = [split[0], split[2]]
         path = path_ref[0]
+        # change to direct reference paths
+        path = path.replace('//iati-activities', '/iati-activities')
+        path = path.replace('//iati-activity', '/iati-activities/iati-activity')
+        path = path.replace('//iati-organisations', '/organisations')
+        path = path.replace('//iati-organisation', '/iati-organisations/iati-organisation')
+
         attribute = path_ref[1]
         name = mapping.find('codelist').attrib['ref']
         file_name = name + '.xml'
@@ -25,26 +30,33 @@ def mapping_to_codelist_rules(mappings):
 
         out = {
             path: {
-                attrkey: attribute,
-                'codelist': file_name,
-                'allowedCodes': allowedCodes
+                attribute: {
+                    'codelist': name,
+                    'allowedCodes': allowedCodes
+                }
             }
-        }
+         }
 
-        # add condition
+        # add condition if present
         if mapping.find('condition') is not None:
-            out[path]['condition'] = mapping.find('condition').text
+            out[path][attribute]['condition'] = mapping.find('condition').text
 
         # add validation rules
         validation_rules = mapping.find('validation-rules')
         if validation_rules is not None:
             for validation_rule in validation_rules:
                 for child in validation_rule:
-                    out[path][child.tag] = child.text  
+                    out[path][attribute][child.tag] = child.text
 
-        yield out
+        existingEntry = data.get(path)
+        if existingEntry is not None:
+            data[path][attribute] = out[path][attribute]  
+        else:
+            data.update(out)
+    return data
 
 
 mappings = ET.parse('mapping.xml')
 with open('codelist_rules.json', 'w') as fp:
-    json.dump(list(mapping_to_codelist_rules(mappings)), fp)
+    data = mapping_to_codelist_rules(mappings)
+    json.dump(data, fp)
