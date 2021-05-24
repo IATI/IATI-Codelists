94a2384bd35c47a6ed5b2972a2eaf61936020e06
nosvalds <niko@devinit.org>
2021-05-24 17:20:21 +0100

    fix linting issues

--- mappings_to_csv_rules.py
+++ mappings_to_csv_rules.py
@@ -1,12 +1,11 @@
 import os
-import json
 from lxml import etree as ET
 import csv
 
 mappings = ET.parse('mapping.xml')
-headers = ['ID','Category','Severity', 'Codelist', 'Message','Context (Xpath)' ]
+headers = ['ID', 'Category', 'Severity', 'Codelist', 'Message', 'Context (Xpath)']
 with open('codelist_rules.csv', 'w') as fp:
-    writer = csv.writer(fp) 
+    writer = csv.writer(fp)
     writer.writerow(headers)
 
     for mapping in mappings.getroot().xpath('//mapping'):
@@ -36,20 +35,14 @@ with open('codelist_rules.csv', 'w') as fp:
             for validation_rule in validation_rules:
                 for child in validation_rule:
                     if (child.tag == 'id'):
-                        ID = child.text 
+                        ID = child.text
                     if (child.tag == 'severity'):
                         severity = child.text
                     if (child.tag == 'category'):
                         category = child.text
                     if (child.tag == 'message'):
                         message = child.text
-                writer.writerow([ ID, category, severity, codelist , message, xpath  ])    
-                
-        
-       
-
-
-
+                writer.writerow([ID, category, severity, codelist, message, xpath])
 
 
 def mapping_to_codelist_rules(mappings):
@@ -82,14 +75,12 @@ def mapping_to_codelist_rules(mappings):
         existingPathAtr = ''
         if existingPath:
             existingPathAtr = data[path].get(attribute) is not None
-        
         out = {
-                path: {
-                    attribute: {
-                    }
+            path: {
+                attribute: {
                 }
             }
-        
+        }
         if (mapping.find('condition') is not None):
             # parse condition xpath
             condition = mapping.find('condition').text
@@ -103,7 +94,7 @@ def mapping_to_codelist_rules(mappings):
             if len(parts) > 1:
                 defaultLink = linkValue
 
-            if not existingPath or not existingPathAtr: 
+            if not existingPath or not existingPathAtr:
                 out[path][attribute]["conditions"] = {}
                 out[path][attribute]["conditions"]["mapping"] = {}
                 out[path][attribute]["conditions"]["linkedAttribute"] = link
@@ -112,15 +103,14 @@ def mapping_to_codelist_rules(mappings):
                 out[path][attribute]["conditions"]["mapping"] = {}
                 out[path][attribute]["conditions"]["linkedAttribute"] = link
             else:
-                out[path][attribute]["conditions"] = data[path][attribute]["conditions"]             
-                        
+                out[path][attribute]["conditions"] = data[path][attribute]["conditions"]
             if defaultLink:
                 out[path][attribute]["conditions"]["defaultLink"] = defaultLink
 
-            out[path][attribute]["conditions"]["mapping"][linkValue] = { "codelist": name, "allowedCodes": allowedCodes }
+            out[path][attribute]["conditions"]["mapping"][linkValue] = {"codelist": name, "allowedCodes": allowedCodes}
         else:
             out[path][attribute]["codelist"] = name
-            out[path][attribute]["allowedCodes"] =  allowedCodes
+            out[path][attribute]["allowedCodes"] = allowedCodes
 
         # add validation rules
         validation_rules = mapping.find('validation-rules')
@@ -131,13 +121,8 @@ def mapping_to_codelist_rules(mappings):
                         out[path][attribute]["conditions"]["mapping"][linkValue][child.tag] = child.text
                     else:
                         out[path][attribute][child.tag] = child.text
-
-        
         if existingPath:
-            data[path][attribute] = out[path][attribute]  
+            data[path][attribute] = out[path][attribute]
         else:
             data.update(out)
     return data
-
-
-
